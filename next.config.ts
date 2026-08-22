99ad8cbb74fde40bb568894c67756c64e3d808d4
yo9aprasetyo <yo9aprasetyo@gmail.com>
2026-08-22 09:45:58 +0700

    fix: clean routes and ready for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Lewati error type checking saat deploy di Vercel
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Lewati warning ESLint saat deploy
-    ignoreDuringBuilds: true,
   },
 }
 
